0da8c1e83fc4820267818da0456f55b03f38240e
Timothy Cocores <timothycocores@gmail.com>
2015-11-18 09:29:43 -0800

    Initial Commit

---
+++ Wedding Party/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wedding Party
+//
+//  Created by Tim.Cocores on 11/18/15.
+//  Copyright © 2015 Coco co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
